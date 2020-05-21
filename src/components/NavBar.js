23be450161cd0149974f519278dcb9bf0d49f8d8
Heidi Boulter <boulter.heidi@gmail.com>
2020-05-21 15:02:39 -0500

    Updated header and imported data

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,67 +10,70 @@ const NavBar = () => {
   };
 
   return (
-    <div className="sidebar">
-      <div className="sidebar__header">
-        <NavLink to="/" exact>
-          <img
-            src="../images/1.jpg"
-            alt="profile_img"
-            className="profile_img"
-          />
-        </NavLink>
-      </div>
-      <div>
-        <ul className="ul">
-          <li className="li">
-            <NavLink
-              to="/about"
-              exact
-              className={"button"}
-              activeStyle={activeStyles}
-            >
-              About
-            </NavLink>
-          </li>
+    <>
+      <div className="header"></div>
+      <div className="sidebar">
+        <div className="sidebar__header">
+          <NavLink to="/" exact>
+            <img
+              src="../images/1.jpg"
+              alt="profile_img"
+              className="profile_img"
+            />
+          </NavLink>
+        </div>
+        <div>
+          <ul className="ul">
+            <li className="li">
+              <NavLink
+                to="/about"
+                exact
+                className={"button"}
+                activeStyle={activeStyles}
+              >
+                About
+              </NavLink>
+            </li>
 
-          <li className="li">
-            <NavLink
-              to="/portfolio"
-              exact
-              className={"button"}
-              activeStyle={activeStyles}
-            >
-              Portfolio
-            </NavLink>
-          </li>
+            <li className="li">
+              <NavLink
+                to="/portfolio"
+                exact
+                className={"button"}
+                activeStyle={activeStyles}
+              >
+                Portfolio
+              </NavLink>
+            </li>
 
-          <li className="li">
-            <NavLink
-              to="/contact"
-              exact
-              className={"button"}
-              activeStyle={activeStyles}
-            >
-              Contact
-            </NavLink>
-          </li>
+            <li className="li">
+              <NavLink
+                to="/contact"
+                exact
+                className={"button"}
+                activeStyle={activeStyles}
+              >
+                Contact
+              </NavLink>
+            </li>
 
-          <li className="li">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="button"
-              href="https://docs.google.com/document/d/1qwWhOlOsGeoSWFcnIxIjDT4njzWPVYgx1WyyEHohsVc/edit?usp=sharing"
-            >
-              Resume
-            </a>
-          </li>
-        </ul>
+            <li className="li">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button"
+                href="https://docs.google.com/document/d/1qwWhOlOsGeoSWFcnIxIjDT4njzWPVYgx1WyyEHohsVc/edit?usp=sharing"
+              >
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="sidebar__footer">
+          <Footer />
+        </div>
       </div>
-      <div className="sidebar__footer">
-        <Footer />
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -1,6 +1,9 @@
 import React from "react";
+import projects from "../components/Projects";
 
 const Portfolio = () => {
+  console.log(projects);
+
   return <div>Portfolio</div>;
 };
 
