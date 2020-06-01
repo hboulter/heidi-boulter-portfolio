8692d4853b79aa715f7d89f51d719e3341359858
Heidi Boulter <boulter.heidi@gmail.com>
2020-06-01 15:49:19 -0500

    Added icon image and title

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -6,13 +6,24 @@ const About = () => {
     <div className="intro">
       <div className="intro-description">
         <p className="about">
-          <span>Intro</span>
-          I'm a "blank" human. I have a background in Education from Ohio State
-          University and Software Engineering.
+          <span>About</span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </p>
 
         <p className="about">
-          <span>Skills</span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
         </p>
       </div>
     </div>
